81dbe390037c2d7fb30e932e5a05a9982f7bc443
zidanindratama <zidanindratama31@gmail.com>
2023-08-03 09:39:03 +0700

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      "primary-gray": "#F9FBFC",
+      "primary-dark": "#0A0E31",
+      "primary-blue-dark": "#1678F2",
+      "primary-purple": "#855FCE",
+      "primary-green": "#27AE60",
+      "primary-green": "#2F80ED",
+      "toska-purple": "#f3effa",
+      "toska-green": "#e9f7ef",
+      "toska-blue": "#eaf2fd",
+      "slightly-darker-gray": "#E2EDFF",
+    },
+  },
+  plugins: [],
+};
