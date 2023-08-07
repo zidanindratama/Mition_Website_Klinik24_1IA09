d1e53c703f3485051b2bd61a8f22d0f0c1e47122
zidanindratama <zidanindratama31@gmail.com>
2023-08-08 05:56:35 +0700

    search

--- src/pages/Dokter.jsx
+++ src/pages/Dokter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { doctors } from "../dev-data/resources";
 import { CardDokter } from "../components";
 import { AiOutlineSearch } from "react-icons/ai";
@@ -6,6 +6,9 @@ import { dokter1 } from "../assets";
 import { Link } from "react-router-dom";
 
 const Dokter = () => {
+  const [search, setSearch] = useState("");
+  console.log(search);
+
   return (
     <div className="w-full bg-primary-gray-bg py-36">
       <div className="xl:max-w-6xl m-auto max-w-[600px] px-4 md:px-0">
@@ -14,15 +17,22 @@ const Dokter = () => {
             type="text"
             placeholder="Cari..."
             className="outline-none bg-[#EAF0F7]"
+            onChange={(e) => setSearch(e.target.value)}
           />
           <span className="hidden md:flex">
             <AiOutlineSearch size={30} style={{ color: "#6D737A" }} />
           </span>
         </div>
         <div className="mt-16 grid md:grid-cols-4 grid-cols-2 gap-12">
-          {doctors.map((doctor) => {
-            return <CardDokter key={doctor.id} {...doctor} />;
-          })}
+          {doctors
+            .filter((doctor) => {
+              return search.toLowerCase() === ""
+                ? doctor
+                : doctor.nama.toLowerCase().includes(search);
+            })
+            .map((doctor) => {
+              return <CardDokter key={doctor.id} {...doctor} />;
+            })}
         </div>
       </div>
     </div>
